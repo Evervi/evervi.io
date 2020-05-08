0b3c66e092d8cfc16a43ee34851b6aee9600b024
xomod <xomod99@gmail.com>
2020-05-09 00:10:47 +0200

    :lipstick: Enhance about section

--- src/pages/Home/About.tsx
+++ src/pages/Home/About.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useLittera } from 'react-littera';
+import translations from '../../translations/pages';
+import Button from '../../components/shared/Button';
 
 function About() {
-    return <p>About us!</p>
+    const [translated, , setLanguage] = useLittera(translations);
+
+    return <>
+        <div className="About">
+            <div className="About__Card">
+                <h3>{translated.about}</h3>
+                <p>{translated.aboutDesc01}</p>
+                <p>Donec pretium lorem metus, ac accumsan lorem auctor sit amet. Praesent sapien tortor, fermentum nec magna sit amet, tincidunt maximus libero.</p>
+                <p>Maecenas ullamcorper pretium sapien ac venenatis. Suspendisse mattis dapibus gravida.</p>
+                <p>Praesent sapien tortor, fermentum nec magna sit amet, tincidunt maximus libero.</p>
+
+                <Button brandColor="primary" onClick={() => setLanguage("pl_PL")}>polski</Button>
+                <Button brandColor="secondary" onClick={() => setLanguage("en_US")}>english</Button>
+
+                <a href="https://google.com">Continue reading ></a>
+            </div>
+
+            <div className="About__Image">
+                <img src="https://source.unsplash.com/aWf7mjwwJJo" />
+            </div>
+
+            <div className="About__Shadow">
+            </div>
+        </div>
+    </>
 }
 
 export default About;
---
+++ src/translations/pages.ts
@@ -0,0 +1,10 @@
+export default {
+    about: {
+        en_US: "About",
+        pl_PL: "O nas"
+    },
+    aboutDesc01: {
+        en_US: "Kamień dry af, consectetur adipiscing elit. Nullam gravida eu lacus ac vulputate. Pellentesque ut magna vitae augue condimentum finibus. Nam ullamcorper scelerisque erat, eget condimentum arcu sagittis ac.",
+        pl_PL: "Pierdzi mucha, consectetur adipiscing elit. Nullam gravida eu lacus ac vulputate. Pellentesque ut magna vitae augue condimentum finibus. Nam ullamcorper scelerisque erat, eget condimentum arcu sagittis ac."
+    },
+}
