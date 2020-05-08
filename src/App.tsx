fe92af7ed4bf79a4d4847c99437bd826d5981ad0
xomod <xomod99@gmail.com>
2020-05-09 00:08:46 +0200

    :package: Add react-littera dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Home from './pages/Home/';
 import Navbar from './components/common/Navbar';
+import LitteraProvider from 'react-littera';
 
 function App() {
+  const [locale, setLocale] = useState('en_US');
+
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-    </div>
+    <LitteraProvider language={locale} setLanguage={setLocale}>
+      <div className="App">
+        <Navbar />
+        <Home />
+      </div>
+    </LitteraProvider>
   );
 }
 
