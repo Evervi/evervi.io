68f4999b0483beb850f1b660a625a248bfbaeee8
xomod <xomod99@gmail.com>
2020-05-08 19:37:10 +0200

    :wrench: Fix warnings

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Offer from './Offer';
 import Header from './Header';
 import About from './About';
 
-import Button from '../../components/shared/Button';
-
 function Home() {
-
-    function handleClick() {
-        
-    }
 
     return (
         <div style={{maxWidth: "100%"}}>
