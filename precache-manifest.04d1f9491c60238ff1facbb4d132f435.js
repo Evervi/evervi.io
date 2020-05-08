8771921ebd551d8d03b4fc1afc6cfc90f0b5f869
xomod <xomod99@gmail.com>
2020-05-08 17:38:34 +0000

    Deploying to gh-pages from  @ 68f4999b0483beb850f1b660a625a248bfbaeee8 🚀

---
+++ precache-manifest.04d1f9491c60238ff1facbb4d132f435.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a6c064c30a6d14a0b993f6ef74f311",
+    "url": "/index.html"
+  },
+  {
+    "revision": "649769fe777af89097be",
+    "url": "/static/css/main.4e6ecbe6.chunk.css"
+  },
+  {
+    "revision": "81033c3a0d5d7934f487",
+    "url": "/static/js/2.49782817.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.49782817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649769fe777af89097be",
+    "url": "/static/js/main.d646b1aa.chunk.js"
+  },
+  {
+    "revision": "cd7a1cd2d4c4336db86b",
+    "url": "/static/js/runtime-main.31f82c75.js"
+  },
+  {
+    "revision": "568c98f92d4651188f8a394caa30279e",
+    "url": "/static/media/undraw_connected_world_wuay.568c98f9.svg"
+  }
+]);
