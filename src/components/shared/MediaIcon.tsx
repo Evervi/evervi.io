c0d18a666265653305b10fe276526ec06120e2d5
xomod <xomod99@gmail.com>
2020-05-10 01:41:22 +0200

    :lipstick: Add hover for MediaIcon

--- src/components/shared/MediaIcon.tsx
+++ src/components/shared/MediaIcon.tsx
@@ -4,6 +4,8 @@ import {ReactComponent as GitHubIcon} from "../../assets/hollow-cut-github.svg";
 import {ReactComponent as TwitterIcon} from "../../assets/hollow-cut-twitter.svg";
 import {ReactComponent as YouTubeIcon} from "../../assets/hollow-cut-youtube.svg";
 
+import cx from 'classnames';
+
 
 type TMedia = "facebook" | "twitter" | "github" | "youtube"; 
 type TMediaMap = { [key in TMedia]: React.FunctionComponent<React.SVGProps<SVGSVGElement>> };
@@ -14,20 +16,16 @@ const MEDIA_IMG_MAP: TMediaMap = Object.freeze({
     "youtube": YouTubeIcon,
 });
 
-const MEDIA_COLOR_MAP: { [key in TMedia]: string } = Object.freeze({
-    "twitter": "#1DA1F2",
-    "facebook": "#0096F6",
-    "github": "#24292E",
-    "youtube": "#F13200",
-});
-
 let key = 0;
 const MediaIcon = ({ media, url }: { media: TMedia, url: string }) => {
     const Img = MEDIA_IMG_MAP[media];
 
+    // Łączy ze sobą stringi.
+    const rootClasses = cx("MediaIcon", `MediaIcon--${media}`);
+
     return <a href={url}>
-            <div className="MediaIcon" key={`media_icon_${key++}`}>
-             <Img className="MediaIcon__Image" style={{ fill: MEDIA_COLOR_MAP[media] }} />
+            <div className={rootClasses} key={`media_icon_${key++}`}>
+             <Img className="MediaIcon__Image" />
             </div>
         </a>
 }
