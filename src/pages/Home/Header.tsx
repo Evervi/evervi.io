0eca3201fe075427d7d754514fbffe23c39b43a9
xomod <xomod99@gmail.com>
2020-05-10 01:40:39 +0200

    :lipstick: Enhance header blob

--- src/pages/Home/Header.tsx
+++ src/pages/Home/Header.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
+import { useLittera } from 'react-littera';
+import translations from '../../translations/pages';
 
 function Header() {
+    const [translated] = useLittera(translations);
+
     return <div className="Header">
         <div className="Header__side Header__side--wide">
             <h1 style={{ fontWeight: "bold", fontSize: "82px", marginTop: 0, marginBottom: "1rem", lineHeight: "70px" }}>
-                <span style={{ opacity: 0.9 }}>wider</span>
+                <span style={{ opacity: 0.9 }}>{translated.header1}</span>
                 <br />
-                <span style={{ color: "#009688" }}>VISION</span>
+                <span style={{ color: "#009688" }}>{translated.header2}</span>
             </h1>
-            <p>We'll take care of your online existence from scratch. We'll take care of every possible detail, making your brand recognizable and present at the highest level. Our offer consists of many treatments that allow you to achieve it - from website preparation to marketing campaigns!</p>
+            <p>{translated.headerDesc}</p>
         </div>
         <div className="Header__side">
+            <div className="Header__side__dotty"></div>
             <div className="Header__side__shape">
                 <div className="Header__side__shadow"></div>
-                <img src="https://source.unsplash.com/tbdAwwKYOMQ/700x700"  alt="header"/>
+                <img src="https://source.unsplash.com/700x700/?it,server,client,connection,code,website"  alt="header"/>
             </div>
+            <p style={{margin: 0, marginTop: "20px", textAlign: "right", float: "right", opacity: 0.5}}>powered by <b>Unsplash</b></p>
         </div>
     </div>
 }
--- src/translations/pages.ts
+++ src/translations/pages.ts
@@ -7,4 +7,16 @@ export default {
         en_US: "Kamień dry af, consectetur adipiscing elit. Nullam gravida eu lacus ac vulputate. Pellentesque ut magna vitae augue condimentum finibus. Nam ullamcorper scelerisque erat, eget condimentum arcu sagittis ac.",
         pl_PL: "Pierdzi mucha, consectetur adipiscing elit. Nullam gravida eu lacus ac vulputate. Pellentesque ut magna vitae augue condimentum finibus. Nam ullamcorper scelerisque erat, eget condimentum arcu sagittis ac."
     },
+    header1: {
+        en_US: "wider",
+        pl_PL: "szersza"
+    },
+    header2: {
+        en_US: "VISION",
+        pl_PL: "WIZJA",
+    },
+    headerDesc: {
+        en_US: "We'll take care of your online existence from scratch. We'll take care of every possible detail, making your brand recognizable and present at the highest level. Our offer consists of many treatments that allow you to achieve it - from website preparation to marketing campaigns!",
+        pl_PL: "Zajmiemy się Twoim istnieniem w sieci od podstaw. Zadbamy o każdy możliwy detal sprawiając, że Twoja marka stanie się rozpoznawalna oraz będzie się prezentować na jak najwyższym poziomie. Nasza oferta składa się z wielu zabiegów, które pozwalają to osiągnąć – od przygotowania witryny internetowej, aż po kampanie marketingowe!"
+    }
 }
