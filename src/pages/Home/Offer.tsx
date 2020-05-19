1e24d76b677bc5168147539a1ebb3343c6689c64
xomod <xomod99@gmail.com>
2020-05-20 00:35:34 +0200

    :lipstick: Improve responsiveness

--- src/pages/Home/Offer.tsx
+++ src/pages/Home/Offer.tsx
@@ -13,14 +13,14 @@ function Offer() {
 function OfferContainer({ title, desc, image_url, icon }: { title: string, desc: string, image_url: string, icon: string}) {
 
   return <div className="Offer__container">
-    <div className="Offer__container__side" style={{ maxWidth: "30%", margin: "0 auto" }}>
+    <div className="Offer__container__side Offer__container__side--text">
       <i className={icon}></i>
       <div style={{flexGrow: 1}}>
         <h3>{title}</h3>
         <p>{desc}</p>
       </div>
     </div> 
-    <div className="Offer__container__side">
+    <div className="Offer__container__side Offer__container__side--image">
       <div className="Offer__container__side__image">
         <img src={image_url} alt="random" />
         <div className="Offer__container__side__image__slope"></div>
