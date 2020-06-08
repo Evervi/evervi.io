d8eab47b384ec0f14ca55288992fdc2081c784bf
xomod <xomod99@gmail.com>
2020-06-09 01:35:03 +0200

    :wrench: Switch to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
 import LitteraProvider from 'react-littera';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Routes from './Routes';
 import Drawer, { DrawerContext } from './components/common/Drawer';
 
