bb8ab4a5a42341d45499783f4cbf2379524def6f
xomod <xomod99@gmail.com>
2020-07-17 11:31:08 +0000

    Deploying to gh-pages from  @ f5348e351a5f7fdc63b187320283666721b58005 🚀

---
+++ precache-manifest.ef453d78cefb2af358da099c61f1f918.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46b76465106629692fc8338ebb32d0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c218ad5fb54501222f8",
+    "url": "/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "19129afd183e3ac27b07",
+    "url": "/static/css/main.9a945254.chunk.css"
+  },
+  {
+    "revision": "9c218ad5fb54501222f8",
+    "url": "/static/js/2.c252e639.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c252e639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19129afd183e3ac27b07",
+    "url": "/static/js/main.bfc7c72f.chunk.js"
+  },
+  {
+    "revision": "aa41426d40a8a8a05bcf",
+    "url": "/static/js/runtime-main.9e8fb671.js"
+  },
+  {
+    "revision": "d1b98023231468fcfda72758286418b2",
+    "url": "/static/media/administrative.d1b98023.jpg"
+  },
+  {
+    "revision": "900abed3b9dcbe496b568d0296b8ce7c",
+    "url": "/static/media/development.900abed3.jpg"
+  },
+  {
+    "revision": "b1c22716833bdaa1260b784a89cb32e9",
+    "url": "/static/media/dotty.b1c22716.svg"
+  },
+  {
+    "revision": "145b3e765b1c65cf92bfe6dc11b1f86f",
+    "url": "/static/media/error_image.145b3e76.svg"
+  },
+  {
+    "revision": "dde57e4a043fe6c898e2c89042e611d0",
+    "url": "/static/media/hollow-cut-facebook.dde57e4a.svg"
+  },
+  {
+    "revision": "833350e30f7dada6279031c70c8d8865",
+    "url": "/static/media/hollow-cut-github.833350e3.svg"
+  },
+  {
+    "revision": "a909c37a156b2c57189b6301010511f0",
+    "url": "/static/media/hollow-cut-twitter.a909c37a.svg"
+  },
+  {
+    "revision": "f8ae759131f786f60d54878bff13f782",
+    "url": "/static/media/hollow-cut-youtube.f8ae7591.svg"
+  },
+  {
+    "revision": "7d7e9b1f731a5699371ac516ed8ee59b",
+    "url": "/static/media/marketing.7d7e9b1f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc1d3fe8fa2cd2e9c4ce8e2d66e0308f.js"
+  "/precache-manifest.ef453d78cefb2af358da099c61f1f918.js"
 );
 
 self.addEventListener('message', (event) => {
